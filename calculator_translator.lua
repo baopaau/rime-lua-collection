21337c3f14d9c81e361a28c24974bd747184a582
baopaau <47582783+baopaau@users.noreply.github.com>
2020-09-06 03:22:48 +0800

    Add files via upload

---
+++ calculator_translator.lua
@@ -0,0 +1,62 @@
+-- Rime Script
+-- 簡易計算器（執行任何Lua表達式）
+-- 格式：=<exp>
+-- 例子：
+-- =1+1 輸出 2
+-- =floor(9^(8/7)*cos(deg(6))) 輸出 -3
+-- =e^pi>pi^e 輸出 true
+-- 需增加 `recognizer/patterns/expression: "^=.*$"`
+
+-- 定義直呼函數（注意命名空間污染）
+abs = math.abs
+acos = math.acos
+asin = math.asin
+atan = math.atan
+ceil = math.ceil
+cos = math.cos
+deg = math.deg
+exp = math.exp
+floor = math.floor
+fmod = math.fmod
+huge = math.huge
+log = math.log
+max = math.max
+maxinteger = math.maxinteger
+min = math.min
+mininteger = math.mininteger
+modf = math.modf
+pi = math.pi
+rad = math.rad
+random = math.random
+randomseed = math.randomseed
+sin = math.sin
+sqrt = math.sqrt
+tan = math.tan
+tointeger = math.tointeger
+type = math.type
+ult = math.ult
+
+e = exp(1)
+ln = math.log
+
+-- greedy：隨時求值（每次變化都會求值，否則結尾爲特定字符時求值）
+local greedy = true
+local function calculator_translator(input, seg)
+  if string.sub(input, 1, 1) ~= "=" then return end
+  
+  local expfin = greedy or string.sub(input, -1, -1) == "E"
+  local exp = (greedy or not expfin) and string.sub(input, 2, -1) or string.sub(input, 2, -2)
+  
+  yield(Candidate("number", seg.start, seg._end, exp, "表達式"))
+       
+  if not expfin then return end
+  
+  -- return語句保證了只有合法的Lua表達式才可執行
+  local result = load("return "..exp)()
+  if result == nil then return end
+  if result == true then result = "true" end
+  if result == false then result = "false" end
+  yield(Candidate("number", seg.start, seg._end, result, "答案"))
+end
+
+return calculator_translator
---
+++ unicode_translator.lua
@@ -0,0 +1,52 @@
+-- Rime Script
+-- Unicode 內碼轉譯（輸出任意Unicode字符）
+-- 格式：U<d|x|o><code>
+-- d、x、o對應十進制、十六進制、八進制
+-- 如 Ux5C13 和 Ud23571 均輸出 `尓`
+local string_char = string.char
+
+local function utf8(cp)
+  if cp < 128 then
+    return string_char(cp)
+  end
+  local suffix = cp % 64
+  local c4 = 128 + suffix
+  cp = (cp - suffix) / 64
+  if cp < 32 then
+    return string_char(192 + cp, c4)
+  end
+  suffix = cp % 64
+  local c3 = 128 + suffix
+  cp = (cp - suffix) / 64
+  if cp < 16 then
+    return string_char(224 + cp, c3, c4)
+  end
+  suffix = cp % 64
+  cp = (cp - suffix) / 64
+  return string_char(240 + cp, 128 + suffix, c3, c4)
+end
+
+local function unicode_translator(input, seg)
+  if string.sub(input, 1, 1) ~= "U" then return end
+  
+  local dict = { d=10, x=16, o=8 }
+  local snd = string.sub(input, 2, 2)
+  local n = dict[snd]
+  if n == nil then return end
+  
+  local str = string.sub(input, 3)
+  local c = tonumber(str, n)
+  if c == nil then return end
+  
+  local fmt = "U"..snd.."%"..(n==16 and "X" or snd)
+  -- 單獨查找
+  yield(Candidate("number", seg.start, seg._end,
+     utf8(c), string.format(fmt, c)))
+  -- 區間查找
+  for i = c*n, c*n+n-1 do
+    yield(Candidate("number", seg.start, seg._end,
+       utf8(i), string.format(fmt, i)))
+  end
+end
+
+return unicode_translator
